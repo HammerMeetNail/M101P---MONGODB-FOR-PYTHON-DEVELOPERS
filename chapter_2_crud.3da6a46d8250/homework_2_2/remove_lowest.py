8a3e47b0993751a911028c486b2331512b35cb6a
Dave <oconnorwork@gmail.com>
2015-01-18 13:27:50 -0500

    Polished remove_lowest.py for homework_2_2

--- chapter_2_crud.3da6a46d8250/homework_2_2/remove_lowest.py
+++ chapter_2_crud.3da6a46d8250/homework_2_2/remove_lowest.py
@@ -2,17 +2,17 @@ import pymongo
 from pymongo import MongoClient
 
 
-# Connect to MongoDB database and set collection
+# Connect to MongoDB database and set collection.
 client = MongoClient('localhost', 27017)
 db = client.students
 collection = db.grades
 
-# Query to locate all homework documents and sort by student_id and score
+# Query to locate all homework documents and sort by student_id and score.
 query = {"type": "homework"}
 selector = [("student_id", pymongo.ASCENDING), ("score", pymongo.ASCENDING)]
 
 
-# Removes lowest homework document from each student_id
+# Removes lowest homework document from each student_id.
 def find_and_remove():
     dropped = {}
     for doc in collection.find(query).sort(selector):
