5e446327c39f1b9d842d3c804cf52133616a5a5f
Dave O'Connor <oconnorwork@gmail.com>
2015-01-25 19:03:48 -0500

    homework3 completed
    blogPostDAO.py updated to insert posts and comments.

--- chapter_3_schema_design.7aab33c35594/homework_3_2/hw3-2and3-3/hw3-2and3-3/blogPostDAO.py
+++ chapter_3_schema_design.7aab33c35594/homework_3_2/hw3-2and3-3/hw3-2and3-3/blogPostDAO.py
@@ -55,6 +55,7 @@ class BlogPostDAO:
         # now insert the post
         try:
             # XXX HW 3.2 Work Here to insert the post
+            self.posts.insert(post)
             print "Inserting the post"
         except:
             print "Error inserting post"
@@ -64,11 +65,8 @@ class BlogPostDAO:
 
     # returns an array of num_posts posts, reverse ordered
     def get_posts(self, num_posts):
-
-        cursor = []         # Placeholder so blog compiles before you make your changes
-
         # XXX HW 3.2 Work here to get the posts
-
+        cursor = self.posts.find().sort('date', direction=-1).limit(num_posts)
         l = []
 
         for post in cursor:
@@ -90,9 +88,8 @@ class BlogPostDAO:
     # find a post corresponding to a particular permalink
     def get_post_by_permalink(self, permalink):
 
-        post = None
+        post = self.posts.find_one({'permalink': permalink})
         # XXX Work here to retrieve the specified post
-
         if post is not None:
             # fix up date
             post['date'] = post['date'].strftime("%A, %B %d %Y at %I:%M%p")
@@ -108,10 +105,8 @@ class BlogPostDAO:
             comment['email'] = email
 
         try:
-            last_error = {'n':-1}           # this is here so the code runs before you fix the next line
-            # XXX HW 3.3 Work here to add the comment to the designated post
-
-
+            last_error = self.posts.update({'permalink': permalink}, {'$push': {'comments': comment}}, upsert=False,
+                                           manipulate=False, safe=True)
             return last_error['n']          # return the number of documents updated
 
         except:
